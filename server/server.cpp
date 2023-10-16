c1eb5d2996da30c5a67b8d9c279260a24884b64c
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-10-16 12:25:04 +0100

    added cgi form interface

--- server/server.cpp
+++ server/server.cpp
@@ -255,7 +255,7 @@ void Server::send_cgibin_response(int fd, int index) {
         dup2(pipefd[1], STDOUT_FILENO);
         dup2(pipefd[1], STDERR_FILENO);
         close(pipefd[1]);
-        std::string path = directory_path + "/get";
+        std::string path = directory_path + "/post";
         char *args[] = {
             (char *)path.c_str(),
             (char *)"hello",
